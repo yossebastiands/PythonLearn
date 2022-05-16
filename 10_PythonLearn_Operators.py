0b00b985a0df80e1dcb9d92737bff2d1ea05f84a
yossebastiands <61390006+yossebastiands@users.noreply.github.com>
2022-05-16 04:32:19 +0200

    Add files via upload

---
+++ 10_PythonLearn_Operators.py
@@ -0,0 +1,19 @@
+# This file is to show the list of operators in Python - from W3SCHOOLS
+# It is better to browse it yourself in W3SCHOOLS.
+
+# Operators are used to perform operations on variables and values.
+# Python divides the operators in the following groups:
+
+# - Arithmetic operators
+# - Assignment operators
+# - Comparison operators
+# - Logical operators
+# - Identity operators
+# - Membership operators
+# - Bitwise operators
+
+# ---------------- ARITHMATIC OPERATORS ----------------------
+# Arithmetic operators are used with numeric values to perform common mathematical operations.
+# Addition (+) : x + y
+print(10+5)
+#
